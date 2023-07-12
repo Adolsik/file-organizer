482b49a4e81a224419e36a49e1571e197a55a4c7
Adrian Uryga <75134645+Adolsik@users.noreply.github.com>
2023-07-12 12:54:41 +0200

    Release 1.0.1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import tkinter
+import tkinter.messagebox
 import tkinter.ttk
 from tkinter import *
 from tkinter import filedialog
 import threading
 import organizer
+import os
 
 root = Tk()
 root.geometry('350x400')
@@ -15,33 +18,52 @@ dir_path = StringVar()
 def main_menu():
     def select_path():
         global dir_path
-        dir_path = filedialog.askdirectory(initialdir='/', title='Select dir',)
-        label_path_info.config(text=f'{dir_path}')
+
+        try:
+            dir_path = filedialog.askdirectory(initialdir='/', title='Select dir',)
+
+            # v1.0.1 file counter
+            def file_counter():
+                counter = 0
+                files = os.listdir(dir_path)
+                for file in files:
+                    if os.path.isfile(os.path.join(dir_path, file)):
+                        counter += 1
+                return counter
+
+            label_file_count.config(text=f'Files: {file_counter()}')
+        except FileNotFoundError:
+            tkinter.messagebox.showerror('Error', 'Directory not found')
+
+        entry_path_info.delete(0, tkinter.END)
+        entry_path_info.insert(0, f'{dir_path}')
         button_start.config(state='normal')
 
     def quit_m():
         root.destroy()
 
     def start():
+        button_start.config(state='disabled')
+
         organize_progress_bar = tkinter.ttk.Progressbar(frame_main, orient=HORIZONTAL, length=300, mode='indeterminate')
         organize_progress_bar.place(x=10, y=140)
 
-        label_info = Label(frame_main, text='Moving files...', font=('italic', 8))
-        label_info.place(x=10, y=170)
-
-        organize_task = threading.Thread(target=organizer.organize, args=(dir_path, organize_progress_bar, label_info,),
-                                         daemon=True)
+        organize_task = threading.Thread(target=organizer.organize, args=(dir_path, organize_progress_bar,
+                                                                          output_text,), daemon=True)
         organize_task.start()
-        button_start.config(state='disabled')
 
     frame_main = LabelFrame(root, padx=10, pady=10)
     frame_main.place(x=2, y=2, height=350, width=348)
 
-    button_select_path = Button(master=frame_main, text='Select path', command=select_path)
+    button_select_path = Button(master=frame_main, text='Select a path', command=select_path)
     button_select_path.place(x=5, y=10, width=100)
 
-    label_path_info = Label(frame_main, text=f'Path: None ', font=('italic', '8'), wraplength=200, justify=LEFT)
-    label_path_info.place(x=113, y=13)
+    entry_path_info = Entry(frame_main, font=('italic', '8'), justify=LEFT)
+    entry_path_info.insert(0, 'Path: None')
+    entry_path_info.place(x=113, y=13, width=200, height=20)
+
+    label_file_count = Label(frame_main, text='Files: 0',  font=('italic', '8'), justify=LEFT)
+    label_file_count.place(x=10, y=40)
 
     button_start = Button(master=frame_main, text='Start', command=start,)
     button_start.place(x=60, y=100, width=200)
@@ -49,6 +71,18 @@ def main_menu():
 
     button_quit = Button(master=root, text="Quit", command=quit_m,)
     button_quit.place(x=80, y=360, height=30, width=200)
+
+    label_output = Label(frame_main, text='Output', font=('italic', 10))
+    label_output.place(x=135, y=140)
+
+    output_text = Text(frame_main, width=50, height=11, font=('italic', 8), state='normal',)
+    output_text.place(x=0, y=165)
+
+    sb = Scrollbar(frame_main)
+    sb.place(x=305, y=165, height=160)
+
+    output_text.config(yscrollcommand=sb.set)
+    sb.config(command=output_text.yview)
 
 
 main_menu_task = threading.Thread(target=main_menu, daemon=True)
--- organizer.py
+++ organizer.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import messagebox
 import threading
 import os
@@ -19,6 +20,7 @@ file_ext_dict = {
     '.psd': "Images",
     '.eps': "Images",
     '.ai': "Images",
+    '.ico': "Images",  # v 1.0.1
     '.mov': "Videos",
     '.wmv': "Videos",
     '.avi': "Videos",
@@ -42,6 +44,8 @@ dir_names = set(file_ext_dict.values())
 
 def organize(path: str, progress_bar, info):
     progress_bar.start()
+    info.config(state='normal')
+    info.delete('1.0', tkinter.END)
 
     # Making folders
     try:
@@ -50,21 +54,33 @@ def organize(path: str, progress_bar, info):
     except FileExistsError:
         messagebox.showwarning('Warning', 'Folders already exist. Delete them and try again.')
         progress_bar.stop()
-        info.config(text='Error occurred. Try again')
+        info.insert(tkinter.END, f'\n Error occurred. Try again \n')
+    info.insert(tkinter.END, '\n Folders created \n')
 
     # Moving files to directories
     files = os.listdir(path)
 
     for file in files:
         if os.path.isfile(os.path.join(path, file)):
-            extension = file[file.find('.'):len(file)]
-            try:
-                dir_to_move = file_ext_dict.get(extension)
-                info.config(text=f'Moving {file}...')
+
+            file_stats = os.stat(os.path.join(path, file))
+            file_size_mb = file_stats.st_size / (1024 * 1024)
+            extension = file[file.find('.'):len(file)].lower()
+
+            # If mp4's size is more than 10MB, app move it into Videos instead of Music
+            if extension == '.mp4' and file_size_mb > 10:
+                dir_to_move = 'Videos'
+                info.insert(tkinter.END, f'\n Moving {file}... Size: {round(file_size_mb,3)} MB \n')
                 shutil.move(os.path.join(path, file), os.path.join(path, dir_to_move))
-            except TypeError:
-                shutil.move(os.path.join(path, file), os.path.join(path, 'Other'))
+            else:
+                try:
+                    dir_to_move = file_ext_dict.get(extension)
+                    info.insert(tkinter.END, f'\n Moving {file}... Size: {round(file_size_mb,3)} MB \n')
+                    shutil.move(os.path.join(path, file), os.path.join(path, dir_to_move))
+                except TypeError:
+                    shutil.move(os.path.join(path, file), os.path.join(path, 'Other'))
 
     progress_bar.stop()
-    info.config(text=f'Success')
-    messagebox.showinfo('Task Completed', 'All files moved successfully')
+    info.config(state='disabled')
+    info.insert(tkinter.END, f'\n Success \n')
+    messagebox.showinfo('Task Completed', 'All files moved into successfully')
