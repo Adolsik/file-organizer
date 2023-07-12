f34130d16f332ffad550b4d2e594fe283310acde
Adrian Uryga <75134645+Adolsik@users.noreply.github.com>
2023-07-12 18:47:45 +0200

    Release 1.0.1b

--- main.py
+++ main.py
@@ -7,15 +7,30 @@ import threading
 import organizer
 import os
 
+
 root = Tk()
-root.geometry('350x400')
+root.geometry('350x450')
 root.title('File Organizer')
 root.resizable(False, False)
+
+root.tk.call("source", "azure.tcl")
+root.tk.call("set_theme", "light")
 
 dir_path = StringVar()
 
 
 def main_menu():
+    # 1.0.1b
+    def change_theme():
+        if root.tk.call("ttk::style", "theme", "use") == "azure-dark":
+            # Set light theme
+            root.tk.call("set_theme", "light")
+            switch_themes.config(text='Dark mode')
+        else:
+            # Set dark theme
+            root.tk.call("set_theme", "dark")
+            switch_themes.config(text='Light mode')
+
     def select_path():
         global dir_path
 
@@ -53,36 +68,45 @@ def main_menu():
         organize_task.start()
 
     frame_main = LabelFrame(root, padx=10, pady=10)
-    frame_main.place(x=2, y=2, height=350, width=348)
+    frame_main.place(x=2, y=40, height=350, width=348)
 
-    button_select_path = Button(master=frame_main, text='Select a path', command=select_path)
+    button_select_path = tkinter.ttk.Button(master=frame_main, text='Select a path', command=select_path)
     button_select_path.place(x=5, y=10, width=100)
 
-    entry_path_info = Entry(frame_main, font=('italic', '8'), justify=LEFT)
+    entry_path_info = tkinter.ttk.Entry(frame_main, font=('italic', '8'), justify=LEFT)
     entry_path_info.insert(0, 'Path: None')
-    entry_path_info.place(x=113, y=13, width=200, height=20)
+    entry_path_info.place(x=113, y=13, width=200, height=25)
 
-    label_file_count = Label(frame_main, text='Files: 0',  font=('italic', '8'), justify=LEFT)
-    label_file_count.place(x=10, y=40)
+    label_file_count = tkinter.ttk.Label(frame_main, text='Files: 0', justify=LEFT)
+    label_file_count.place(x=10, y=47)
 
-    button_start = Button(master=frame_main, text='Start', command=start,)
+    button_start = tkinter.ttk.Button(master=frame_main, text='Start', command=start,)
     button_start.place(x=60, y=100, width=200)
     button_start.config(state='disabled')
 
-    button_quit = Button(master=root, text="Quit", command=quit_m,)
-    button_quit.place(x=80, y=360, height=30, width=200)
+    button_quit = tkinter.ttk.Button(master=root, text="Quit", command=quit_m,)
+    button_quit.place(x=80, y=400, height=30, width=200)
 
-    label_output = Label(frame_main, text='Output', font=('italic', 10))
+    label_output = tkinter.ttk.Label(frame_main, text='Output', font=('italic', 10))
     label_output.place(x=135, y=140)
 
-    output_text = Text(frame_main, width=50, height=11, font=('italic', 8), state='normal',)
+    output_text = Text(frame_main, width=52, height=11, font=('italic', 8), state='normal',)
     output_text.place(x=0, y=165)
 
-    sb = Scrollbar(frame_main)
-    sb.place(x=305, y=165, height=160)
+    sb_output = tkinter.ttk.Scrollbar(frame_main, orient='vertical')
+    sb_output.place(x=315, y=165, height=158)
 
-    output_text.config(yscrollcommand=sb.set)
-    sb.config(command=output_text.yview)
+    output_text.config(yscrollcommand=sb_output.set)
+    sb_output.config(command=output_text.yview)
+
+    sb_path = tkinter.ttk.Scrollbar(frame_main, orient='horizontal')
+    sb_path.place(x=113, y=40, height=20, width=200)
+
+    switch_themes = tkinter.ttk.Checkbutton(root, text='Dark mode', style='Switch.TCheckbutton', command=change_theme)
+    switch_themes.place(x=0, y=5)
+
+    entry_path_info.config(xscrollcommand=sb_path.set)
+    sb_path.config(command=entry_path_info.xview)
 
 
 main_menu_task = threading.Thread(target=main_menu, daemon=True)
--- organizer.py
+++ organizer.py
@@ -66,21 +66,23 @@ def organize(path: str, progress_bar, info):
             file_stats = os.stat(os.path.join(path, file))
             file_size_mb = file_stats.st_size / (1024 * 1024)
             extension = file[file.find('.'):len(file)].lower()
+            info.see('end')
 
             # If mp4's size is more than 10MB, app move it into Videos instead of Music
             if extension == '.mp4' and file_size_mb > 10:
                 dir_to_move = 'Videos'
-                info.insert(tkinter.END, f'\n Moving {file}... Size: {round(file_size_mb,3)} MB \n')
+                info.insert(tkinter.END, f'\n Moving {file} into {dir_to_move} \n Size: {round(file_size_mb,3)} MB \n')
                 shutil.move(os.path.join(path, file), os.path.join(path, dir_to_move))
             else:
                 try:
                     dir_to_move = file_ext_dict.get(extension)
-                    info.insert(tkinter.END, f'\n Moving {file}... Size: {round(file_size_mb,3)} MB \n')
+                    info.insert(tkinter.END, f'\n Moving {file} into {dir_to_move} \n Size: {round(file_size_mb,3)} MB \n')
                     shutil.move(os.path.join(path, file), os.path.join(path, dir_to_move))
                 except TypeError:
                     shutil.move(os.path.join(path, file), os.path.join(path, 'Other'))
 
     progress_bar.stop()
+    info.see('end')
+    info.insert(tkinter.END, f'\n Success! \n')
     info.config(state='disabled')
-    info.insert(tkinter.END, f'\n Success \n')
     messagebox.showinfo('Task Completed', 'All files moved into successfully')
