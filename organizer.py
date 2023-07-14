92359c938ae8ae2849a277452fc3540da8371e35
Adrian Uryga <75134645+Adolsik@users.noreply.github.com>
2023-07-14 14:03:44 +0200

    Release 1.0.2b

--- organizer.py
+++ organizer.py
@@ -25,19 +25,9 @@ def organize(path: str, progress_bar, info):
     progress_bar.start()
     info.config(state='normal')
     info.delete('1.0', tkinter.END)
+    info.insert(tkinter.END, f'Organizing files in {path} \n')
 
-    # Making folders
-    try:
-        for dir_name in dir_names:
-            os.mkdir(path + '/' + dir_name)
-    except FileExistsError:
-        messagebox.showwarning('Warning', 'Folders already exist. Delete them and try again.')
-        progress_bar.stop()
-        info.insert(tkinter.END, f'\n Error occurred. Try again \n')
-        return 0
-    info.insert(tkinter.END, '\n Folders created \n')
-
-    # Moving files to directories
+    # Organize files
     files = os.listdir(path)
 
     for file in files:
@@ -49,10 +39,22 @@ def organize(path: str, progress_bar, info):
             info.see('end')
             try:
                 dir_to_move = file_ext_dict.get(extension)
+
+                # Create labeled folder
+                is_exist = os.path.exists(path + '/' + dir_to_move)
+                if not is_exist:
+                    os.mkdir(path + '/' + dir_to_move)
+                    info.insert(tkinter.END, f'\n Folder {dir_to_move} created  \n')
+
+                # Moving files to directories
                 info.insert(tkinter.END, f'\n Moving {file} into {dir_to_move} \n Size: {round(file_size_mb, 3)}'
                                          f' MB \n')
                 shutil.move(os.path.join(path, file), os.path.join(path, dir_to_move))
             except TypeError:
+                is_exist = os.path.exists(path + '/Other')
+                if not is_exist:
+                    os.mkdir(path + '/Other')
+                    info.insert(tkinter.END, f'\n Folder Other created  \n')
                 shutil.move(os.path.join(path, file), os.path.join(path, 'Other'))
 
     progress_bar.stop()
